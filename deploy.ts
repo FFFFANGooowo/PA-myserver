391c926fe27c9eef6a56a193aba4d0358cec441b
SammFang <yingfang2011owo@gmail.com>
2025-03-09 12:03:16 +0800

    Update deploy.ts

--- deploy.ts
+++ deploy.ts
@@ -1551,7 +1551,7 @@ serve(async (req) => {
 </head>
 <body>
     <div class="header">
-        <div class="title">队伍情况</div>
+        <div class="title"><p style="text-indent: 12px;">队伍情况</p></div>
         <div class="byline">samm-paserver</div>
     </div>
     <div class="queue-container" id="queueContainer">
